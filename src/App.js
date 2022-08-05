5646947ad226960543073d919b5bd2167751a73e
Kalitha <kalithad@mobitel.lk>
2022-08-05 17:23:38 +0530

    added Protected routes

--- src/App.js
+++ src/App.js
@@ -2,33 +2,39 @@ import './App.css';
 import React from "react";
 import {faComments} from '@fortawesome/free-solid-svg-icons';
 import {BrowserRouter, Switch, Route, HashRouter} from "react-router-dom";
-import { library } from '@fortawesome/fontawesome-svg-core';
+import {library} from '@fortawesome/fontawesome-svg-core';
 import LandingPage from "./components/landingPage/index";
 import Login from "./components/login";
 import AdminHomePage from "./components/adminHomePage";
 import UserHomePage from "./components/userHomePage";
+import { Provider } from 'react-redux';
+import { store } from './store';
+import ProtectedRoute from "./utility/ProtectedRoutes";
+
 library.add(
     faComments
 )
 
 function App() {
     return (
-        <BrowserRouter >
-            <Switch>
-                <Route exact path={"/"}>
-                    <LandingPage/>
-                </Route>
-                <Route exact path={"/login"}>
-                    <Login/>
-                </Route>
-                <Route exact path={"/adminHome"}>
-                    <AdminHomePage/>
-                </Route>
-                <Route exact path={"/userHome"}>
-                    <UserHomePage/>
-                </Route>
-            </Switch>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path={"/"}>
+                        <LandingPage/>
+                    </Route>
+                    <Route exact path={"/login"}>
+                        <Login/>
+                    </Route>
+                    <ProtectedRoute exact path={"/adminHome"}>
+                        <AdminHomePage/>
+                    </ProtectedRoute>
+                    <ProtectedRoute exact path={"/userHome"}>
+                        <UserHomePage/>
+                    </ProtectedRoute>
+                </Switch>
+            </BrowserRouter>
+        </Provider>
 
     );
 }
---
+++ src/utility/ProtectedRoutes/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Redirect, Route} from "react-router-dom";
+import {useSelector} from "react-redux";
+
+function ProtectedRoute  ({children, ...rest}) {
+    const role = useSelector(store => store.auth.roleType);
+    const {pathname} = rest.location;
+    let canAccess = false;
+    if (pathname.startsWith("/adminHome") && (role === "admin" )) {
+        canAccess =  true;
+    } else if (pathname.startsWith("/userHome") && role === "user") {
+        canAccess =  true;
+    } else {
+        canAccess = false;
+    }
+    return (
+
+        <Route {...rest} render={() => { return canAccess? children : <Redirect to='/login'/>}}/>
+    );
+
+}
+
+export default ProtectedRoute;
